3e070323cca2848d730780c5f200fca88b0626d2
Diego Matheus <diegoamatheus@gmail.com>
2020-10-18 18:55:15 -0400

    Cleanup

--- src/Routes/Landing.js
+++ src/Routes/Landing.js
@@ -2,7 +2,8 @@ import React from "react";
 import {Switch, Route} from "react-router";
 import CustomEditor from '../Components/Editor/index';
 import Login from "../Components/Login";
-import Home from '../Components/Home/Home'
+import Home from '../Components/Home/Home';
+
 const Landing = (props) => {
     return (
         <React.Fragment>
@@ -11,16 +12,6 @@ const Landing = (props) => {
                 <Route path={'/login'} component={Login}/>
                 <Route path={'/editor'} component={CustomEditor}/>
             </Switch>
-        </React.Fragment>
-    )
-}
-
-const _Home = (props) => {
-    return (
-        <React.Fragment>
-            <section className={"home_section"}>
-                <h1>Home</h1>
-            </section>
         </React.Fragment>
     )
 }
