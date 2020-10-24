f958d021caaefeb1c98d5c1853cd93be5a3eacf3
Diego Matheus <diegoamatheus@gmail.com>
2020-10-24 13:25:28 -0400

    Cleanup login form, missing reusable components for form labels

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,9 +6,20 @@ const Login = (props) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isEmail, setIsEmail] = useState(true);
+    const EMAIL_MESSAGE = "Email is not valid";
 
     const onEmailChange = (event) => {
-        setEmail(event.target.value)
+
+        let email = event.target.value;
+
+        if(email.indexOf("@") == -1){
+            return setIsEmail(false);
+        }
+
+        setEmail(email)
+        setIsEmail(true);
+
     }
 
     const onPasswordChange = (event) => {
@@ -17,15 +28,20 @@ const Login = (props) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        return alert('submitting form!');
+        return console.log(email, password)
     }
 
     return (
         <React.Fragment>
-            <section>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" name="email" id="email" className={"input"} onChange={onEmailChange}/>
-                    <label htmlFor="email">Email</label>
+            <section className="form-wrapper">
+                <form onSubmit={handleSubmit} className={"form"}>
+                    <input type="email" name="email" id="email" className={"input"} onChange={onEmailChange} style={{
+                        borderColor: isEmail ? "green" : "red",
+                    }}/>
+                    {isEmail ?
+                        <label htmlFor="email">Email</label> :
+                        (<label>{EMAIL_MESSAGE}</label>)
+                    }
                     <input type="password" name="password" id="password" className={"input"} onChange={onPasswordChange}/>
                     <label htmlFor="password">Password</label>
                     <Button variant={"contained"} color={"primary"} type="submit" className={"submit_btn"}>Log in</Button>
